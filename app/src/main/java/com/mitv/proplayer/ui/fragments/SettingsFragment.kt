251dd7a691e99fd2d48df6b5dab1f34c2339dd49
1113marcio-alt <1113marcio@gmail.com>
2025-10-11 16:02:37 +0200

    Create SettingsFragment.kt

---
+++ app/src/main/java/com/mitv/proplayer/ui/fragments/SettingsFragment.kt
@@ -0,0 +1,16 @@
+package com.mitv.proplayer.ui.fragments
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.mitv.proplayer.R
+
+class SettingsFragment : Fragment(R.layout.fragment_settings) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        
+        // Aqui vamos adicionar as configurações do app
+        // igual ao MyTVOnline 3
+    }
+}
