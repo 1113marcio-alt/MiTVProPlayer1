7884d26647ff78fd9f21a38b2801d6632f8b68c1
1113marcio-alt <1113marcio@gmail.com>
2025-10-11 16:02:11 +0200

    Create PlayerController.kt

---
+++ app/src/main/java/com/mitv/proplayer/utils/PlayerController.kt
@@ -0,0 +1,59 @@
+package com.mitv.proplayer.utils
+
+import android.content.Context
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
+import com.mitv.proplayer.models.Channel
+
+class PlayerController(context: Context) {
+    
+    var exoPlayer: ExoPlayer? = null
+    private var currentChannel: Channel? = null
+    
+    init {
+        initializePlayer(context)
+    }
+    
+    private fun initializePlayer(context: Context) {
+        val trackSelector = DefaultTrackSelector(context).apply {
+            setParameters(buildUponParameters().setMaxVideoSizeSd())
+        }
+        
+        exoPlayer = ExoPlayer.Builder(context)
+            .setTrackSelector(trackSelector)
+            .build()
+    }
+    
+    fun playChannel(channel: Channel) {
+        currentChannel = channel
+        
+        exoPlayer?.let { player ->
+            val mediaItem = MediaItem.fromUri(channel.url)
+            player.setMediaItem(mediaItem)
+            player.prepare()
+            player.play()
+        }
+    }
+    
+    fun pause() {
+        exoPlayer?.pause()
+    }
+    
+    fun resume() {
+        exoPlayer?.play()
+    }
+    
+    fun stop() {
+        exoPlayer?.stop()
+    }
+    
+    fun release() {
+        exoPlayer?.release()
+        exoPlayer = null
+    }
+    
+    fun isPlaying(): Boolean {
+        return exoPlayer?.isPlaying ?: false
+    }
+}
