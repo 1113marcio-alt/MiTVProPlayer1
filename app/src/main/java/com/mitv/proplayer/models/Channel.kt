ae5a35037bc7906ae31bcd56c83680cba33f7e78
1113marcio-alt <1113marcio@gmail.com>
2025-10-11 16:47:51 +0200

    Update Channel.kt

--- app/src/main/java/com/mitv/proplayer/models/Channel.kt
+++ app/src/main/java/com/mitv/proplayer/models/Channel.kt
@@ -2,9 +2,12 @@ package com.mitv.proplayer.models
 
 data class Channel(
     val id: String,
-    val name: String,
-    val url: String,
+    var name: String,
+    var url: String,
     val logo: String? = null,
     val group: String? = null,
     val isFavorite: Boolean = false
-)
+) {
+    // Construtor vazio para Firebase etc.
+    constructor() : this("", "", "", null, null, false)
+}
