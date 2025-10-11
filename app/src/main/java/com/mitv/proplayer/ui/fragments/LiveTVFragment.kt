feba53b249f416de9c0e2e778904a1878be0ac9e
1113marcio-alt <1113marcio@gmail.com>
2025-10-11 15:54:49 +0200

    Create LiveTVFragment.kt

---
+++ app/src/main/java/com/mitv/proplayer/ui/fragments/LiveTVFragment.kt
@@ -0,0 +1,16 @@
+package com.mitv.proplayer.ui.fragments
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.mitv.proplayer.R
+
+class LiveTVFragment : Fragment(R.layout.fragment_live_tv) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        
+        // Aqui vamos adicionar a lista de canais ao vivo
+        // igual ao MyTVOnline 3
+    }
+}
