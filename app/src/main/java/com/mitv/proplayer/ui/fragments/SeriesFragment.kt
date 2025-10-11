db7a31812053cbd1fdac05978f46cb8d0e5f3603
1113marcio-alt <1113marcio@gmail.com>
2025-10-11 15:59:33 +0200

    Create SeriesFragment.kt

---
+++ app/src/main/java/com/mitv/proplayer/ui/fragments/SeriesFragment.kt
@@ -0,0 +1,15 @@
+package com.mitv.proplayer.ui.fragments
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.mitv.proplayer.R
+
+class SeriesFragment : Fragment(R.layout.fragment_series) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        
+        // Aqui vamos adicionar as séries igual MyTVOnline
+    }
+}
