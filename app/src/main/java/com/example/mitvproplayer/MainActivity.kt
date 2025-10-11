172c0d07e50d305d2006c5523b42f7b6815f3ba8
1113marcio-alt <1113marcio@gmail.com>
2025-10-11 21:48:22 +0200

    Update MainActivity.kt

--- app/src/main/java/com/example/mitvproplayer/MainActivity.kt
+++ app/src/main/java/com/example/mitvproplayer/MainActivity.kt
@@ -2,34 +2,11 @@ package com.example.mitvproplayer
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        setupNavigation()
-    }
-
-    private fun setupNavigation() {
-        // Configuração do NavController
-        val navHostFragment = supportFragmentManager
-            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        navController = navHostFragment.navController
-
-        // Configuração da BottomNavigationView (se existir)
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
-        bottomNavigationView?.setupWithNavController(navController)
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
