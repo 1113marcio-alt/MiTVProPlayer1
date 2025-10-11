1783122fc088ccf7ef99bd704ffb6122822c7a23
1113marcio-alt <1113marcio@gmail.com>
2025-10-11 15:56:00 +0200

    Create VODFragment.kt

---
+++ app/src/main/java/com/mitv/proplayer/ui/fragments/VODFragment.kt
@@ -0,0 +1,15 @@
+package com.mitv.proplayer.ui.fragments
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.mitv.proplayer.R
+
+class VODFragment : Fragment(R.layout.fragment_vod) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        
+        // Aqui vamos adicionar os filmes e VOD
+    }
+}
