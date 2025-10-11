e84d8c168e1c15b8e41c437fff72aa7d5b90bfe0
1113marcio-alt <1113marcio@gmail.com>
2025-10-11 15:40:52 +0200

    Create MainActivity.kt

---
+++ app/src/main/java/com/mitv/proplayer/ui/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.mitv.proplayer.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.mitv.proplayer.databinding.ActivityMainBinding
+import com.mitv.proplayer.ui.fragments.HomeFragment
+
+class MainActivity : AppCompatActivity() {
+    
+    private lateinit var binding: ActivityMainBinding
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        
+        setupHomeFragment()
+    }
+    
+    private fun setupHomeFragment() {
+        val fragment = HomeFragment()
+        supportFragmentManager.beginTransaction()
+            .replace(binding.fragmentContainer.id, fragment)
+            .commit()
+    }
+}
